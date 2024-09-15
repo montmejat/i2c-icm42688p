c58b16b7dcb5e41b58bf3fe9e02dcff0af3ace8d
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2024-09-15 17:22:05 +0200

    Roll and pitch estimation

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import signal
+from math import tanh
 
 import gpiod
 import rerun as rr
@@ -22,6 +23,10 @@ if __name__ == "__main__":
 
     print("Starting...")
 
+    gyro_roll = 0
+    gyro_pitch = 0
+    prev_time = None
+
     with gpiod.request_lines(
         "/dev/gpiochip0", config={4: gpiod.LineSettings(edge_detection=Edge.RISING)}
     ) as request:
@@ -36,3 +41,22 @@ if __name__ == "__main__":
                 rr.log("imu/gyro/x", rr.Scalar(imu.gyro[0]))
                 rr.log("imu/gyro/y", rr.Scalar(imu.gyro[1]))
                 rr.log("imu/gyro/z", rr.Scalar(imu.gyro[2]))
+
+                roll = tanh(imu.accel[1] / imu.accel[2])
+                pitch = tanh(-imu.accel[0] / 9.81)
+
+                rr.log("accel_estimate/roll", rr.Scalar(roll))
+                rr.log("accel_estimate/pitch", rr.Scalar(pitch))
+
+                if prev_time is None:
+                    prev_time = event.timestamp_ns
+                    continue
+
+                dt = (event.timestamp_ns - prev_time) / 1e9
+                prev_time = event.timestamp_ns
+
+                gyro_roll += imu.gyro[0] * dt
+                gyro_pitch += imu.gyro[1] * dt
+
+                rr.log("gyro_estimate/roll", rr.Scalar(gyro_roll))
+                rr.log("gyro_estimate/pitch", rr.Scalar(gyro_pitch))
